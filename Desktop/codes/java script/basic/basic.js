be5814ca410372fef5c4ef9902657ad82183bfa0
sujalgera01 <sujalgera01@gmail.com>
2020-04-07 01:18:12 +0530

    changes

--- Desktop/codes/java script/basic/basic.js
+++
@@ -1,5 +0,0 @@
-var userfirstName = prompt("Enter your First Name: ");
-var userLastName = prompt("Enter your Last Name: ");
-var userAge = prompt("Enter your Age: ");
-alert("Your full name is " + userfirstName + " " + userLastName);
-alert("Your age is " + userAge);
