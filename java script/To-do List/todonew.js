d80f1111a33ea7f06c0c7cdcd1944537a0421182
sujalgera01 <sujalgera01@gmail.com>
2020-04-18 10:49:34 +0530

    change todos

---
+++ java script/To-do List/todonew.js
@@ -0,0 +1,42 @@
+var todos = [];
+
+var input = prompt("What would you like to do?");
+
+while(input !== "quit"){
+
+if(input === "list"){
+	displayTodo();
+} 
+
+else if(input === "new"){
+	addTodo();
+}
+
+else if(input === "delete"){
+	delTodo();
+}
+
+input = prompt("What would you like to do?")
+}
+console.log("You have Quit!"); 
+
+
+function displayTodo(){
+	console.log("********");
+	todos.forEach(function(todo,i){
+		console.log(i + ": " + todo);
+	});
+	console.log("********");
+}
+
+function addTodo(){
+	var newTodo = prompt("Enter new Todo");
+	todos.push(newTodo);
+	console.log("Added Todo");
+}
+
+function delTodo(){
+	var index = prompt("Enter the index of Todo to delete");
+	todos.splice(index,1);
+	console.log("Deleted Todo");
+}
