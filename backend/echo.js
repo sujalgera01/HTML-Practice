434c9bdeff75f1ad2cf9d75672dcb2f5fd7fae07
sujalgera01 <sujalgera01@gmail.com>
2020-06-24 22:15:59 +0530

    practice for node

---
+++ backend/echo.js
@@ -0,0 +1,10 @@
+//Make a fn that takes 2 arg (str and num) and print the str as much time the value of num.
+
+function echo(str,num){
+    for(var i=0; i<=num; i++){
+        console.log(str);
+    }
+}
+
+echo("Echo!!",10);
+echo('hello!!!',3);
