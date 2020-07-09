23a16b3e7108c113a4cb075acf88ff5551cd5706
sujalgera01 <sujalgera01@gmail.com>
2020-07-09 21:04:00 +0530

    rendering

--- backend/renderHTML/app.js
+++ backend/renderHTML/app.js
@@ -3,8 +3,13 @@ var app = express();
 
 app.get("/",function(req,res){
     res.render("home.ejs");
+});
+
+app.get("/friend/:name",function(req,res){
+    var name = req.params.name;
+    res.render("friend.ejs",{names: name});
 })
 
 app.listen("3000",function(){
     console.log("server started...");
-})
+});
