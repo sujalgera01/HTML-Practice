867e9c1f68c4a98cb41ca3d854427449ec7071fc
sujalgera01 <sujalgera01@gmail.com>
2020-07-10 13:31:00 +0530

    loops

--- backend/renderHTML/app.js
+++ backend/renderHTML/app.js
@@ -8,7 +8,17 @@ app.get("/",function(req,res){
 app.get("/friend/:name",function(req,res){
     var name = req.params.name;
     res.render("friend.ejs",{names: name});
-})
+});
+
+app.get("/posts",function(req,res){
+    var posts = [
+        {title: "Post1", author: "Sujal"},
+        {title: "Post2", author:  "Anmol"},
+        {title: "Post3", author:  "mohit"},
+        {title: "Post4", author:  "Mradul"},
+    ];
+    res.render("post.ejs", {post: posts});
+});
 
 app.listen("3000",function(){
     console.log("server started...");
